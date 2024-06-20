0fbcac0ce0671c2002883153a8b43b43e3c93644
Pradeep Muthaiya <pradeep.muthaiya@berkeley.edu>
2024-06-20 16:07:00 -0700

    Update machine_learning.py

--- machine_learning.py
+++ machine_learning.py
@@ -27,6 +27,36 @@ class MachineLearning(object):
         df_fit['rating'] = ratings
         return df_fit.to_dict('records')
 
+    def RandomForestRegression(self, new_train):
+        regr = RandomForestRegressor(max_depth=2, random_state=0)
+        regr.fit(self.df[self.X_train_cols], self.df[self.y_train_col])
+        df_fit = pd.DataFrame(new_train)
+        df_fit_pred = df_fit[self.X_train_cols]
+        ratings = regr.predict(df_fit_pred)
+        ratings = [item for sublist in ratings for item in sublist]
+        df_fit['rating'] = ratings
+        return df_fit.to_dict('records')
+
+    def NeuralNetwork(self, new_train):
+        regr = MLPRegressor(random_state=1, max_iter=500).fit(
+            self.df[self.X_train_cols], self.df[self.y_train_col])
+        df_fit = pd.DataFrame(new_train)
+        df_fit_pred = df_fit[self.X_train_cols]
+        ratings = regr.predict(df_fit_pred)
+        ratings = [item for sublist in ratings for item in sublist]
+        df_fit['rating'] = ratings
+        return df_fit.to_dict('records')
+
+    def SVM(self, new_train):
+        regr = make_pipeline(StandardScaler(), SVR(C=1.0, epsilon=0.2))
+        regr.fit(self.df[self.X_train_cols], self.df[self.y_train_col])
+        df_fit = pd.DataFrame(new_train)
+        df_fit_pred = df_fit[self.X_train_cols]
+        ratings = regr.predict(df_fit_pred)
+        ratings = [item for sublist in ratings for item in sublist]
+        df_fit['rating'] = ratings
+        return df_fit.to_dict('records')
+
     def kNN(self, new_train):
         neigh = KNeighborsRegressor(n_neighbors=2)
         neigh.fit(self.df[self.X_train_cols], self.df[self.y_train_col])
