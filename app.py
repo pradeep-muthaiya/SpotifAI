731bceed2fe5d30bae078e9b9f5d13042f8184f0
Pradeep Muthaiya <pradeep.muthaiya@berkeley.edu>
2024-06-20 16:06:25 -0700

    Update app.py

--- app.py
+++ app.py
@@ -9,74 +9,213 @@ import pandas as pd
 import plotly.express as px
 from sklearn.linear_model import LinearRegression
 
-# open up id.txt for the client id and secret
 with open(sys.argv[1]) as f:
     mylist = f.read().splitlines()
 
-# assign the id and secret as variables
 client_id = mylist[0]
 client_secret = mylist[1]
 
-# create our spotify_API instance
 spotify_API = spotify.SpotifyAPI(client_id, client_secret)
 
 
 user_key = 'track'
-# Fill in what columns and values we want to take in from each set
+# Setup table.
 columns_input = ['track', 'artist', 'album', 'rating']
-columns_store = []
-columns_display = []
+columns_store = ['Song', 'Song Id', 'Artist', 'Artist Id',
+                 'Album', 'Album Id', 'danceability', 'energy',
+                 'key', 'loudness', 'mode', 'speechiness', 'acousticness',
+                 'instrumentalness', 'liveness', 'valence', 'tempo',
+                 'duration_ms', 'time signature', 'genres', 'rating']
+columns_display = ['Song', 'Song Id', 'Artist', 'Album', 'rating', 'danceability', 'energy',
+                   'key', 'loudness', 'mode', 'speechiness', 'acousticness',
+                   'instrumentalness', 'liveness', 'valence', 'tempo',
+                   'duration_ms', 'time signature']
 
 
 table = dash_table.DataTable(
-    columns=[{"name": column, "id": column} for column in columns_display], data=[], id="table")
+    columns=[{"name": column, "id": column} for column in columns_display], data=[], id="table",
+    style_data={
+        'overflow': 'hidden',
+        'textOverflow': 'ellipsis',
+        'minWidth': '50px', 'width': '50px', 'maxWidth': '100px'
+    }, style_table={
+        'width': '100%'
+    })
 
 table2 = dash_table.DataTable(
-    columns=[{"name": column, "id": column} for column in columns_display], data=[], id="table2")
+    columns=[{"name": column, "id": column} for column in columns_display], data=[], id="table2",
+    style_data={
+        'overflow': 'hidden',
+        'textOverflow': 'ellipsis',
+        'minWidth': '50px', 'width': '50px', 'maxWidth': '100px'
+    }, style_table={
+        'width': '100%'
+    })
 
-# Create dash app.
+# Create app.
 app = dash.Dash(prevent_initial_callbacks=True)
-
-# Create the dash app layout. Add more values to the layout including titles, input states, input button, and then seperate input buttons for songs not included in the playlist
-app.layout = html.Div([table, table2])
+app.layout = html.Div([html.H1("SpotifAI")] + 
+                      [dcc.Input(id=column, value=column) for column in columns_input] +
+                      [html.Button("Save", id="save"), dcc.Store(id="cache", data=[]), dcc.Store(id='new_songs', data=[]),
+                       dcc.Graph(id='display_avg'), table,
+                       html.H2("Generate Songs"), html.Button(
+                          "Generate", id="generate"), dcc.Input(id='gen_n', type='number', value=5),
+                       html.Div(), table2])
 
 
 # takes in the cache data and args from input and adds new song to cache data
-@app.callback(Output("table", "data"), [Input("save", "n_clicks")],
-              [Input("table", "data")],
+@app.callback(Output("cache", "data"), [Input("save", "n_clicks")], [State("cache", "data")] +
               [State(column, "value") for column in columns_input])
-def append(save, data, *args):
+def append(n_clicks, data, *args):
 
-    # take in inputs of given arguments, and current table data and search for song
+    # make the list of args into dictionary
+    record = {columns_input[i]: arg for i, arg in enumerate(list(args))}
 
-    # from song get the features of the song
+    # get rating value out of dict
+    rating = record['rating']
+    # delete rating value so dict can be used in query params for API call
+    del record['rating']
 
-    # get data from the table
+    # delete the apostrophe in tracks as it messes with API calls
+    for key in record:
+        if "'" in record[key]:
+            record[key] = record[key].replace("'", "")
 
-    # add the new information to the data
+    # call spotify API to search
+    response = spotify_API.search(record, search_type="track")
+    #print('response:', response)
 
-    # once its been added, send the data back to the table
+    # if search fails:
+    if response['tracks']['total'] == 0:
+        return False
 
-    return
+    # make df row
+    row = spotify_API.get_df_row(response['tracks']['items'][0])
+    row.append(int(rating))
+    print('row:', row)
+
+    # append new row to overall data
+    data.append({columns_store[i]: arg for i, arg in enumerate(row)})
+    # Return the updated data.
+    return data
+
+# updates table based on cache data
 
 
-@app.callback(Output("table2", "data"), [Input("generate", "n_clicks")], [Input("table2", "data")],
-              [Input("table1", "data")], [State(column, "value") for column in columns_input])
-def generate_values(generate, data, data2, *args):
+@app.callback(Output("table", "data"), Output("display_avg", "figure"), [Input("cache", "data")])
+def updated_table(data):
+    # print(data)
+    temp_df = pd.DataFrame(data)
+    temp_df = temp_df.drop(
+        ['Artist Id', 'Album Id', 'genres'], axis=1)
 
-    # take in inputs of given arguments, and current table data and search for song
+    cols = ['danceability', 'energy', 'speechiness',
+            'acousticness', 'instrumentalness', 'liveness', 'valence']
 
-    # from song get the features of the song
+    means = []
+    for i in cols:
+        means.append(temp_df[i].mean())
 
-    # get data from table1
+    new_df = pd.DataFrame({'Features': cols, 'Average Value': means})
+    fig = px.bar(new_df, x='Features', y='Average Value')
+    return temp_df.to_dict('records'), fig
 
-    # using table data, and new song, perform ml using machinelearning.py
 
-    # add the new information to table2
+@app.callback(Output("table2", "data"), Output("new_songs", "data"), Input("generate", "n_clicks"),
+              State("gen_n", "value"), State("cache", "data"), State("new_songs", "data"))
+def generate_values(n_clicks, n, data, gen_data):
+    # How to Generate New Tracks
+    artists, genres, songs = [], [], []
+    for i in data[:5]:
+        artists.append(i['Artist Id'])
+        genres.append(i['genres'])
+        songs.append(i['Song Id'])
 
-    # once its been added, send the data back to the table
+    # have to limit to 5 TOTAL seed values
 
-    return
+    # EDIT CODE HERE ------
+    artists = artists[:2]
+    songs = songs[:1]
+
+    if len(artists) > 1:
+        artists = ','.join(artists)
+    else:
+        artists = artists[0]
+    if len(genres) > 1:
+        genres = [item for sublist in genres for item in sublist]
+        genres = [genres[0]]
+        if len(genres) > 1:
+            genres = list(set(genres))
+            genres = ','.join(genres)
+        else:
+            genres = genres[0]
+    else:
+        genres = genres[0]
+    if len(songs) > 1:
+        songs = ','.join(songs)
+    else:
+        songs = songs[0]
+
+    print('artists:', artists)
+    print('genres:', genres)
+    print('songs:', songs)
+
+    # Get recommended tracks json file
+    response = spotify_API.get_recommended_tracks(
+        artists, genres, songs, n)
+    #print('response:', response)
+
+    # END CODE EDITTING SECTION ----
+
+    # find the values of each recommended track and add to gen_data
+    for i in range(n):
+        df_row = spotify_API.get_df_row(response['tracks'][i])
+        gen_data.append(
+            {columns_store[i]: arg for i, arg in enumerate(df_row)})
+
+    # Machine Learning
+
+    # EDIT CODE HERE -------
+    df_train = pd.DataFrame(data)
+    print('df_train:', df_train)
+    X_train = df_train[['danceability', 'energy',
+                        'key', 'loudness', 'mode', 'speechiness', 'acousticness',
+                        'instrumentalness', 'liveness', 'valence', 'tempo',
+                        'duration_ms', 'time signature']]
+    y_train = df_train[['rating']]
+
+    print('X_train:', X_train)
+    print('y_train:', y_train)
+
+    regressor = LinearRegression()
+    regressor.fit(X_train, y_train)
+    print('coef:', regressor.coef_)
+
+    print('gendata:', gen_data)
+    df_fit = pd.DataFrame(gen_data)
+    print('df_fit:', df_fit)
+
+    df_fit_pred = df_fit[['danceability', 'energy',
+                          'key', 'loudness', 'mode', 'speechiness', 'acousticness',
+                          'instrumentalness', 'liveness', 'valence', 'tempo',
+                          'duration_ms', 'time signature']]
+    print('df_fit2:', df_fit_pred)
+
+    ratings = regressor.predict(df_fit_pred)
+    print('ratings1:', ratings)
+
+    # END CODE EDITING ---
+
+    ratings = [item for sublist in ratings for item in sublist]
+    print('ratings2:', ratings)
+
+    df_fit['rating'] = ratings
+    print('df_fit3:', df_fit)
+
+    gen_data = df_fit.to_dict('records')
+    print('gendata2:', gen_data)
+
+    return gen_data, gen_data
 
 
 if __name__ == '__main__':
